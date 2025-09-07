7ea9df86f9156a7517c8877d43c6d22495f724a0
JimmysRanch <alexandersiskind@gmail.com>
2025-09-07 01:03:25 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-}
-
-export default nextConfig
+const nextConfig = {};
+export default nextConfig;
